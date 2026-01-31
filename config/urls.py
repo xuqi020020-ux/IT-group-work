8eaf0301d2650c1ccc88ace2c5379bfde8b49d4e
xuqi <xuqiqi@yeah.net>
2026-01-31 12:46:26 +0000

    feat: add accounts login/logout routes and login page

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
     path('', include('core.urls')),
 ]
 """config URL Configuration
