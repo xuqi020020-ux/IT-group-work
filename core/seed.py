56e629b5797df82e3f89719fd1af76bb388a6bb7
xuqi <xuqiqi@yeah.net>
2026-02-14 11:17:55 +0000

    update seed data

---
+++ core/seed.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+
+DEFAULT_USERS = [
+    ("yaoting", "yaoting@10086", True, True),   # admin
+    ("test-p-01", "groupmember01", False, False),
+    ("test-p-02", "groupmember02", False, False),
+    ("test-p-03", "groupmember03", False, False),
+]
+
+def seed_default_users():
+    for username, password, is_staff, is_superuser in DEFAULT_USERS:
+        if User.objects.filter(username=username).exists():
+            continue
+        u = User.objects.create_user(username=username, password=password)
+        u.is_staff = is_staff
+        u.is_superuser = is_superuser
+        u.save()
+
+
